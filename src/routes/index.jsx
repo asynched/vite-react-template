a09c685928dba783737281611d055bb95ae6a05e
Eder Lima <devilsouls666@gmail.com>
2022-01-22 01:15:52 -0300

    fix: import order for the router

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
-import Loading from '@/components/Loading'
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Loading from '@/components/Loading'
 
 const IndexPage = React.lazy(() => import('@/pages/IndexPage'))
 
