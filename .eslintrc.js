64718285b4afafe6553241bdd4a37f448a05de30
Eder Lima <devilsouls666@gmail.com>
2022-01-22 03:05:26 -0300

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {},
+  ignore: ['tailwind.config.js', 'postcss.config.js', '.eslintrc.js'],
+}
